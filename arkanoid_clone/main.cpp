7f74535d8e9482eff2bab1e528ca03438254818e
touhf <tobytubar@gmail.com>
2023-05-03 14:00:20 +0500

    added paddle

--- arkanoid_clone/main.cpp
+++ arkanoid_clone/main.cpp
@@ -7,6 +7,7 @@ using namespace sf;
 // constexpr - immutable compile-time value
 constexpr int windowWidth {800}, windowHeight {600};
 constexpr float ballRadius {10.f}, ballVelocity {8.f};
+constexpr float paddleWidth {60.f}, paddleHeight {20.f}, paddleVelocity {6.f};
 
 struct Ball
 {
@@ -43,12 +44,48 @@ struct Ball
     float bottom() { return y() + shape.getRadius(); }
 };
 
+struct Paddle
+{
+    RectangleShape shape;
+    Vector2f velocity;
+
+    Paddle(float mX, float mY)
+    {
+        shape.setPosition(mX, mY);
+        shape.setSize({paddleWidth, paddleHeight});
+        shape.setFillColor(Color::White);
+        shape.setOrigin(paddleWidth / 2.f, paddleHeight / 2.f);
+    }
+
+    void update()
+    {
+        shape.move(velocity);
+
+        // moving and keeping paddle inside window
+        if (Keyboard::isKeyPressed(Keyboard::Key::Left) &&
+                left() > 0) velocity.x = -paddleVelocity;
+        else if (Keyboard::isKeyPressed(Keyboard::Key::Right) &&
+                right() < windowWidth) velocity.x = paddleVelocity;
+        
+        // if no input staying in one place
+        else velocity.x = 0;
+    }
+
+    float x() { return shape.getPosition().x; }
+    float y() { return shape.getPosition().y; }
+    float left()   { return x() - shape.getSize().x / 2.f; }
+    float right()  { return x() + shape.getSize().x / 2.f; }
+    float top()    { return y() - shape.getSize().y / 2.f; }
+    float bottom() { return y() + shape.getSize().y / 2.f; }
+};
+
 int main()
 {
     RenderWindow window {{windowWidth, windowHeight}, "Arkanoid - 1"};
     window.setFramerateLimit(60);
 
     Ball ball{windowWidth/2, windowHeight/2};
+    Paddle paddle {windowWidth/2, windowHeight - 50};
 
     while (window.isOpen())
     {
@@ -60,8 +97,13 @@ int main()
         }
         // clear window from previously drawn graphics
         window.clear(Color::Black);
+
         ball.update();
+        paddle.update();
+
         window.draw(ball.shape);
+        window.draw(paddle.shape);
+
         window.display();
     }
 }
