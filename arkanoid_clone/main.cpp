7c0fdd9f6b816c76eefcadefd20bd22d85a39bfc
touhf <tobytubar@gmail.com>
2023-05-03 17:03:56 +0500

    drawing bricks

--- arkanoid_clone/main.cpp
+++ arkanoid_clone/main.cpp
@@ -9,6 +9,10 @@ constexpr int windowWidth {800}, windowHeight {600};
 constexpr float ballRadius {10.f}, ballVelocity {8.f};
 constexpr float paddleWidth {60.f}, paddleHeight {20.f}, paddleVelocity {6.f};
 
+// constants for bricks
+constexpr float blockWidth {60.f}, blockHeight {20.f};
+constexpr int countBlocksX {11}, countBlocksY {4};
+
 struct Ball
 {
     CircleShape shape;
@@ -79,6 +83,28 @@ struct Paddle
     float bottom() { return y() + shape.getSize().y / 2.f; }
 };
 
+struct Brick
+{
+    RectangleShape shape;
+
+    bool destroyed {false};
+
+    Brick (float mX, float mY)
+    {
+        shape.setPosition(mX, mY);
+        shape.setSize({blockWidth, blockHeight});
+        shape.setFillColor(Color::Yellow);
+        shape.setOrigin(blockWidth / 2.f, blockHeight / 2.f);
+    }
+
+    float x() { return shape.getPosition().x; }
+    float y() { return shape.getPosition().y; }
+    float left()   { return x() - shape.getSize().x / 2.f; }
+    float right()  { return x() + shape.getSize().x / 2.f; }
+    float top()    { return y() - shape.getSize().y / 2.f; }
+    float bottom() { return y() + shape.getSize().y / 2.f; }
+};
+
 // generic function to check collision between two shapes
 template<class T1, class T2> bool isIntersecting(T1& mA, T2& mB)
 {
@@ -106,6 +132,14 @@ int main()
     Ball ball{windowWidth/2, windowHeight/2};
     Paddle paddle {windowWidth/2, windowHeight - 50};
 
+    vector<Brick> bricks;
+
+    // filling bricks
+    for (int iX {0}; iX < countBlocksX; ++iX)
+        for (int iY {0}; iY < countBlocksY; ++iY)
+            bricks.emplace_back((iX + 1) * (blockWidth + 3) + 22,
+                                (iY + 2) * (blockHeight + 3));
+
     while (window.isOpen())
     {
         Event event;
@@ -125,6 +159,9 @@ int main()
         window.draw(ball.shape);
         window.draw(paddle.shape);
 
+        // drawing bricks
+        for (auto& brick : bricks) window.draw(brick.shape);
+    
         window.display();
     }
 }
