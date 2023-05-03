4a934fa26d74486aea1f36210b0c0dfb54d96489
touhf <tobytubar@gmail.com>
2023-05-03 18:20:21 +0500

    collision and removing bricks

--- arkanoid_clone/main.cpp
+++ arkanoid_clone/main.cpp
@@ -124,6 +124,40 @@ void checkCollision(Paddle& mPaddle, Ball& mBall)
     else mBall.velocity.x = ballVelocity;
 }
 
+void checkCollision(Brick& mBrick, Ball& mBall)
+{
+    // if no collision return
+    if (!isIntersecting(mBrick, mBall)) return;
+
+    // otherwise brick got hit
+    mBrick.destroyed = true;
+
+    // calculating how much ball intersects the brick in every direction
+    float overlapLeft   {mBall.right() - mBrick.left()};
+    float overlapRight  {mBrick.right() - mBrick.left()};
+    float overlapTop    {mBall.bottom() - mBrick.top()};
+    float overlapBottom {mBrick.bottom() - mBall.top()};
+
+    // if megnitude of the left overlap is smaller than the right we can assume
+    // the ball hit brick from the left etc...
+    bool ballFromLeft(abs(overlapLeft) < abs(overlapRight));
+    bool ballFromTop(abs(overlapTop) < abs(overlapBottom));
+
+    // let's store the miniumum overlaps for the X and Y axes
+    float minOverlapX {ballFromLeft ? overlapLeft : overlapRight};
+    float minOverlapY {ballFromTop ? overlapTop : overlapBottom};
+    
+    // if the magnitude of the X overlap is less than the magnitude
+    // of the Y ovelap we can assume the ball hit the brick
+    // horizontally - otherwise vertically
+
+    // change ball velocity based on calcs above
+    if (abs(minOverlapX) < abs(minOverlapY))
+        mBall.velocity.x = ballFromLeft ? -ballVelocity : ballVelocity;
+    else
+        mBall.velocity.y = ballFromTop ? -ballVelocity : ballVelocity;
+}
+
 int main()
 {
     RenderWindow window {{windowWidth, windowHeight}, "Arkanoid - 1"};
@@ -161,6 +195,14 @@ int main()
 
         // drawing bricks
         for (auto& brick : bricks) window.draw(brick.shape);
+
+        // checking ball and brick collisions (for each brick)
+        for (auto& brick : bricks) checkCollision(brick, ball);
+
+        // removing destroyed bricks
+        bricks.erase(remove_if(begin(bricks), end(bricks),
+                    [](const Brick& mBrick) { return mBrick.destroyed; }),
+                    end(bricks));
     
         window.display();
     }
